fcc4ce3e32dc7a139506d9548c380b91d8de5f13
Cristi Teodorescu <cristian.teodorescu97@yahoo.ro>
2022-11-30 13:09:14 +0200

    Add first unit test following TDD concept

---
+++ RoomBookingApp.Core.Tests/RoomBookingRequestProcesor.cs
@@ -0,0 +1,14 @@
+﻿namespace RoomBookingApp.Core.Tests
+{
+    internal class RoomBookingRequestProcesor
+    {
+        public RoomBookingRequestProcesor()
+        {
+        }
+
+        internal RoomBookingResult BookRoom(RoomBookingRequest bookingRequest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RoomBookingApp.Core.Tests/RoomBookingRequestProcesorTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Shouldly;
+
+namespace RoomBookingApp.Core.Tests
+{
+    public class RoomBookingRequestProcesorTest
+    {
+        [Fact]
+        public void ShouldReturn_RoomBookingResponse_WithValues()
+        {
+            //Arrange
+
+            var request = new RoomBookingRequest
+            {
+                FullName = "Test Name",
+                Email = "test@request.com",
+                Date = new DateTime(2022, 10, 20)
+            };
+
+            var processor = new RoomBookingRequestProcesor();
+
+            //Act
+
+            RoomBookingResult result = processor.BookRoom(request);
+
+            //Assert
+
+            Assert.NotNull(result);
+            Assert.Equal(request.FullName, result.FullName);
+            Assert.Equal(request.Email, result.Email);
+            Assert.Equal(request.Date, result.Date);
+
+            result.ShouldNotBeNull();
+            result.FullName.ShouldBe(request.FullName);
+            result.Email.ShouldBe(request.Email);
+            result.Date.ShouldBe(request.Date);
+        }
+    }
+}
