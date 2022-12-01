dbcea9f3ffcc996910c56181a079d32c6a3199df
Cristi Teodorescu <cristian.teodorescu97@yahoo.ro>
2022-12-01 20:41:37 +0200

    Test and implement add booking id to result

--- RoomBookingApp.Core/Processors/RoomBookingRequestProcessor.cs
+++ RoomBookingApp.Core/Processors/RoomBookingRequestProcessor.cs
@@ -30,6 +30,8 @@ namespace RoomBookingApp.Core.Processors
                 var roomBooking = CreateRoomBookingObject<RoomBooking>(bookingRequest);
                 roomBooking.RoomId = room.Id;
                 _roomBookingService.Save(roomBooking);
+
+                result.RoomBookingId= roomBooking.Id;
                 result.Flag = BookingResultFlag.Success;
             }
             else
