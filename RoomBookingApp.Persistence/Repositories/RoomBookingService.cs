cd0390885ac90884e3d31b0eba6b5128bc587e55
Cristi Teodorescu <cristian.teodorescu97@yahoo.ro>
2022-12-01 22:11:10 +0200

    implement available rooms and add integration test for this step

--- RoomBookingApp.Persistence/Repositories/RoomBookingService.cs
+++ RoomBookingApp.Persistence/Repositories/RoomBookingService.cs
@@ -10,9 +10,15 @@ namespace RoomBookingApp.Persistence.Repositories
 {
     public class RoomBookingService : IRoomBookingService
     {
+        private readonly RoomBookingAppDbContext _context;
+
+        public RoomBookingService(RoomBookingAppDbContext context)
+        {
+            this._context = context;
+        }
         public IEnumerable<Room> GetAvailableRooms(DateTime date)
         {
-            throw new NotImplementedException();
+            return _context.Rooms.Where(q => q.RoomBookings.Any(x=>x.Date == date) == false).ToList();
         }
 
         public void Save(RoomBooking roomBooking)
